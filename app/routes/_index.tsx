1fdb9c1459d67d615f0d94cfc2ab2792d36faa4f
christoph-codes <tkcwebdev@gmail.com>
2023-11-30 00:35:29 -0800

    2: Add previous work to home page with actual images

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -117,34 +117,34 @@ export default function Index() {
 					className="my-8"
 				>
 					<img
-						src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+						src="/work/roseman3.png"
 						alt="hey"
-						className="rounded-lg w-full"
+						className="rounded-lg w-full object-cover"
 					/>
 					<img
-						src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+						src="/work/roseman5.png"
 						alt="hey"
-						className="rounded-lg w-full"
+						className="rounded-lg w-full object-cover"
 					/>
 					<img
-						src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+						src="/work/roseman6a.png"
 						alt="hey"
-						className="rounded-lg w-full"
+						className="rounded-lg w-full object-cover"
 					/>
 					<img
-						src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+						src="/work/roseman9.png"
 						alt="hey"
-						className="rounded-lg w-full"
+						className="rounded-lg w-full object-cover"
 					/>
 					<img
-						src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+						src="/work/safari_hoa_middle_island.png"
 						alt="hey"
-						className="rounded-lg w-full"
+						className="rounded-lg w-full object-cover"
 					/>
 					<img
-						src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+						src="/work/safari_hoa_west_corner.png"
 						alt="hey"
-						className="rounded-lg w-full"
+						className="rounded-lg w-full object-cover"
 					/>
 				</Grid>
 			</Section>
