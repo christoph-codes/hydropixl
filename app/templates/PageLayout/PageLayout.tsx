1104172de93499ce55c880392ffb7abab3770a1e
christoph-codes <tkcwebdev@gmail.com>
2023-11-29 22:31:14 -0800

    2: Create page layout template for all pages

--- app/templates/PageLayout/PageLayout.tsx
+++ app/templates/PageLayout/PageLayout.tsx
@@ -2,13 +2,14 @@ import type { PropsWithChildren } from "react";
 import { Box } from "@radix-ui/themes";
 import Header from "~/components/Header";
 import type { BoxProps } from "node_modules/@radix-ui/themes/dist/esm/components/box";
+import Footer from "~/components/Footer";
 
 const PageLayout = ({ children, ...rest }: PropsWithChildren & BoxProps) => {
 	return (
 		<div {...rest}>
 			<Header />
 			<Box className="min-h-[70vh]">{children}</Box>
-			<div>footer</div>
+			<Footer />
 		</div>
 	);
 };
