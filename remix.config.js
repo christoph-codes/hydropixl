f53eada356462584cf8747416cde1efb21000ce2
christoph-codes <tkcwebdev@gmail.com>
2023-12-08 18:34:05 -0800

    17: try to fix build errors

--- remix.config.js
+++ remix.config.js
@@ -2,8 +2,8 @@
 export default {
 	ignoredRouteFiles: ["**/.*"],
 	serverDependenciesToBundle: ["@radix-ui/themes"], // https://github.com/remix-run/remix/discussions/7494
-	// appDirectory: "app",
-	// assetsBuildDirectory: "public/build",
-	// publicPath: "/build/",
-	// serverBuildPath: "build/index.js",
+	appDirectory: "app",
+	assetsBuildDirectory: "public/build",
+	publicPath: "/build/",
+	serverBuildPath: "build/index.js",
 };
